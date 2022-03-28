1b6d7292b60f9b5306f01920fcde6a47cdaadb0e
amcombes <99033736+steelfish1010@users.noreply.github.com>
2022-03-28 21:45:48 +0100

    refactors to async...await

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,16 +1,15 @@
 const db = require("../db/connection");
 
-exports.fetchArticleById = (article_id) => {
+exports.fetchArticleById = async (article_id) => {
   if (!parseInt(article_id)) {
     return Promise.reject({ status: 400, msg: "article_id is not a number" });
   }
-  return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
-    .then((res) => {
-      if (res.rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Invalid article_id" });
-      } else {
-        return res.rows[0];
-      }
-    });
+  const res = await db.query("SELECT * FROM articles WHERE article_id = $1", [
+    article_id,
+  ]);
+  if (res.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Invalid article_id" });
+  } else {
+    return res.rows[0];
+  }
 };
